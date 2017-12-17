f61c7a9201d20d4e0631bdd399b8c85dc2dcfc7c
Park JeHo <linuxpark@gmail.com>
2017-12-17 20:16:10 +0900

    modify F_CPU macro before delay.h

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
  * Created: Sun Dec 10 KST 2017
  * Author : jeho park<linuxpark@gmail.com>
  */ 
+#undef F_CPU
+#define F_CPU 16000000UL
+
 #include <avr/io.h>
 #include <stdio.h>
 #include <avr/interrupt.h>
@@ -10,9 +13,6 @@
 #include "i2c.h"
 #include "uart.h"
 #include "ms5611.h"
-
-#undef F_CPU
-#define F_CPU 16000000UL
 
 int main(void) {
 	int ret;
