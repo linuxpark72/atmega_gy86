14dd7d4467ac025c28652d3496a7c9ea4a14a793
Park JeHo <linuxpark@gmail.com>
2017-12-24 20:49:30 +0900

    change the calling function prototype bcoz the ms5611 api was changed.

--- main.c
+++ main.c
@@ -16,8 +16,7 @@
 
 int main(void) {
 	int ret;
-	struct coeff_ms5611 ms_coe;
-	uint32_t d1, d2;
+	int32_t pressure, temperature;
 	uint32_t count = 0;
 	
 	stdout = &OUTPUT;
@@ -29,7 +28,7 @@ int main(void) {
 	dprint("initialized...\n\r");
 
 	_delay_ms(500);
-	ret = ms5611_reset();
+	ret = ms5611_init();
 	if (ret) {
 		dprint("Failed ms5611_reset()\n\r");
 		goto exit;
@@ -39,22 +38,13 @@ int main(void) {
 
 	while(1) {
 		
-		d1 = d2 = 0;
-		ms_coe.c1 = ms_coe.c2 = ms_coe.c3 = ms_coe.c4 = ms_coe.c5 = ms_coe.c6 = 0;
-		ret = ms5611_get_coeff(&ms_coe);
+		ret = ms5611_get_pressure(MS5611_OSR_4096, &pressure, &temperature);
 		if (ret) {
-			dprint("Failed ms5611_get_coeff(): %d\n\r", ret);
+			dprint("Failed ms5611_get_pressure: %d\n\r", ret);
 			goto exit;
 		}
 
-		ret = ms5611_get_ds(MS5611_OSR_4096, &d1, &d2);
-		if (ret) {
-			dprint("Failed ms5611_get_coeff(): %d\n\r", ret);
-			goto exit;
-		}
-
-		dprint("%ld: D1(%ld), D2(%ld), c1(%d), c2(%d) c3(%d), c4(%d), c5(%d) c6(%d)\n\r", 
-			   count++, d1, d2, ms_coe.c1, ms_coe.c2, ms_coe.c3, ms_coe.c4, ms_coe.c5, ms_coe.c6);
+		dprint("%ld: D1(%ld), D2(%ld)\n\r", count++, pressure, temperature);
 		_delay_ms(1000);
 	};
 
