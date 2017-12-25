ac4907a28ec895fb790c65727c1b28b5e5609586
Park JeHo <linuxpark@gmail.com>
2017-12-25 19:15:25 +0900

    rearrange dprint

--- main.c
+++ main.c
@@ -15,39 +15,35 @@
 #include "ms5611.h"
 
 int main(void) {
-	int ret;
 	int32_t pressure, temperature;
-	uint32_t count = 0;
+	int ret;
 	
 	stdout = &OUTPUT;
-	stdin = &INPUT;
 
 	uart_init();
 	i2c_init();
 	sei();
-	dprint("initialized...\n\r");
+	dprintf("initialized...\n\r");
 
-	_delay_ms(500);
 	ret = ms5611_init();
+	_delay_ms(100);
 	if (ret) {
-		dprint("Failed ms5611_reset()\n\r");
+		dprintf("Failed ms5611_reset()\n\r");
 		goto exit;
 	} else {
-		dprint("success on ms5611_reset()\n\r");
+		dprintf("success on ms5611_init()\n\r");
 	}
 
 	while(1) {
 		
 		ret = ms5611_get_pressure(MS5611_OSR_4096, &pressure, &temperature);
 		if (ret) {
-			dprint("Failed ms5611_get_pressure: %d\n\r", ret);
+			dprintf("Failed ms5611_get_pressure: %d\n\r", ret);
 			goto exit;
 		}
-
-		dprint("%ld: D1(%ld), D2(%ld)\n\r", count++, pressure, temperature);
 		_delay_ms(1000);
 	};
 
 exit:
-	dprint("exit...\n\r");
+	dprintf("exit...\n\r");
 }
