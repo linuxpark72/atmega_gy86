454c4feaed13c750604f1ed0584b911e7e07a9fe
Park JeHo <linuxpark@gmail.com>
2018-01-01 01:00:23 +0900

    changed the api calling style according to the change of ms5611 api

--- main.c
+++ main.c
@@ -3,47 +3,58 @@
  * Created: Sun Dec 10 KST 2017
  * Author : jeho park<linuxpark@gmail.com>
  */ 
-#undef F_CPU
-#define F_CPU 16000000UL
-
 #include <avr/io.h>
 #include <stdio.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <math.h>
 #include "i2c.h"
 #include "uart.h"
 #include "ms5611.h"
 
 int main(void) {
-	int32_t pressure, temperature;
-	int ret;
-	
+	unsigned int n_prom[8]; // calibration coefficients
+	double P;               // compensated pressure value
+	double T;               // compensated temperature value
+	unsigned char n_crc;  // crc value of the prom
 	stdout = &OUTPUT;
+	unsigned int ret;
 
 	uart_init();
 	i2c_init();
 	sei();
-	dprintf("initialized...\n\r");
 
-	ret = ms5611_init();
-	_delay_ms(100);
-	if (ret) {
-		dprintf("Failed ms5611_reset()\n\r");
-		goto exit;
-	} else {
-		dprintf("success on ms5611_init()\n\r");
+	dprintf("initialized...\n\r");
+	ret = ms5611_reset();              // reset IC
+	if (!ret) {
+		dprintf("failed to ms5611_reset(): %d\n\r", ret);
+		return 0;
 	}
 
-	while(1) {
-		
-		ret = ms5611_get_pressure(MS5611_OSR_4096, &pressure, &temperature);
-		if (ret) {
-			dprintf("Failed ms5611_get_pressure: %d\n\r", ret);
-			goto exit;
-		}
-		_delay_ms(1000);
-	};
+	ret = ms5611_get_coeffs(n_prom);   // get coefficient 
+	if (!ret) {
+		dprintf("failed to ms5611_get_coeffs(): %d\n\r", ret);
+		return 0;
+	}
 
-exit:
+	n_crc = ms5611_cal_crc4(n_prom);  // calculate the CRC
+	// TODO: check crc 
+	dprintf("(%d)[(%d),(%d),(%d),(%d),(%d),(%d)](%d),c(%d)\n\r",
+			n_prom[0], n_prom[1], n_prom[2], n_prom[3], 
+			n_prom[4], n_prom[5], n_prom[6], n_prom[7], n_crc);
+
+	for(;;) 
+	{
+		// calculate P, T with the coefficients
+		ret = ms5611_get_pt(n_prom, &P, &T);
+		if (!ret) {
+			dprintf("failed to ms5611_get_pt(): %d\n\r", ret);
+			continue;
+		}
+		dprintf("T(%5.2f), P(%5.2f)\n", T, P);
+		_delay_ms(500);
+	}
+
 	dprintf("exit...\n\r");
+	return 0;
 }
