30e44ab38310f620a5e915444a49ddd3d6be779a
Park JeHo <linuxpark@gmail.com>
2018-01-01 02:09:17 +0900

    remove obsolete script

--- docs/objcp_fush.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-############################################
-prog_name=$1
-dev_name=/dev/tty.usbserial-DN018JNZ
-
-avr-objcopy -j .text -j .data -O binary ${prog_name}.elf  ${prog_name}.bin
-avrdude -p atmega128 -c stk500 -P ${dev_name} -U flash:w:${prog_name}.bin:r 
