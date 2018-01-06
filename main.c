21fa19102aaca3d00395dffe67dabdb506cff89c
Park JeHo <linuxpark@gmail.com>
2018-01-06 20:18:18 +0900

    change debug format

--- main.c
+++ main.c
@@ -7,7 +7,6 @@
 #include <stdio.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include <math.h>
 #include "i2c.h"
 #include "uart.h"
 #include "ms5611.h"
@@ -22,39 +21,38 @@ int main(void) {
 
 	uart_init();
 	i2c_init();
-	sei();
 
-	dprintf("initialized...\n\r");
+	dprintf("\ninitialized...\n");
 	ret = ms5611_reset();              // reset IC
 	if (!ret) {
-		dprintf("failed to ms5611_reset(): %d\n\r", ret);
+		dprintf("failed to ms5611_reset(): %d\n", ret);
 		return 0;
 	}
 
 	ret = ms5611_get_coeffs(n_prom);   // get coefficient 
 	if (!ret) {
-		dprintf("failed to ms5611_get_coeffs(): %d\n\r", ret);
+		dprintf("failed to ms5611_get_coeffs(): %d\n", ret);
 		return 0;
 	}
 
 	n_crc = ms5611_cal_crc4(n_prom);  // calculate the CRC
-	// TODO: check crc 
-	dprintf("(%d)[(%d),(%d),(%d),(%d),(%d),(%d)](%d),c(%d)\n\r",
-			n_prom[0], n_prom[1], n_prom[2], n_prom[3], 
-			n_prom[4], n_prom[5], n_prom[6], n_prom[7], n_crc);
 
-	for(;;) 
-	{
+	for(;;) {
 		// calculate P, T with the coefficients
 		ret = ms5611_get_pt(n_prom, &P, &T);
 		if (!ret) {
-			dprintf("failed to ms5611_get_pt(): %d\n\r", ret);
+			dprintf("failed to ms5611_get_pt(): %d\n", ret);
 			continue;
 		}
-		dprintf("T(%5.2f), P(%5.2f)\n", T, P);
-		_delay_ms(500);
+		
+		dprintf("\n0[%d], 1[%d], 2[%d], 3[%d], 4[%d], 5[%d], 6[%d], 7[%d],"
+				" CRC(0x%x)\nTemperature(%d), Pressure(%d mbar)\n\n",
+				n_prom[0], n_prom[1], n_prom[2], n_prom[3], 
+				n_prom[4], n_prom[5], n_prom[6], n_prom[7], 
+				n_crc, (signed int)T,  (signed int)P);
+		_delay_ms(1000);
 	}
 
-	dprintf("exit...\n\r");
+	dprintf("exit...\n");
 	return 0;
 }
