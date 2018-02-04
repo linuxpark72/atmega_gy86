037e5c914093883f161528a8604749988bc0a28f
Park JeHo <linuxpark@gmail.com>
2018-02-04 19:51:53 +0900

    remove some ugly named macro ex) ADDR_W/R

--- ms5611.h
+++ ms5611.h
@@ -12,8 +12,12 @@
 #define FALSE           0
 
 #define MS5611_ADDR  0XEE
+#if 0
 #define ADDR_W       MS5611_ADDR + I2C_WRITE
 #define ADDR_R       MS5611_ADDR + I2C_READ
+#endif
+#define MS5611_ADDR_W MS5611_ADDR + I2C_WRITE
+#define MS5611_ADDR_R MS5611_ADDR + I2C_READ
 
 #define CMD_RESET    0x1E
 #define CMD_ADC_READ 0x00
