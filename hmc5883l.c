8460aa28d68feeaeed9e70d0d9d45f44c6a03046
Park JeHo <linuxpark@gmail.com>
2018-02-05 01:47:37 +0900

    add setup function for cra, crb, mr

--- hmc5883l.c
+++ hmc5883l.c
@@ -9,59 +9,82 @@
 #include "uart.h"
 #include "hmc5883l.h"
 
-static unsigned int hmc5883l_i2c_send(char cmd) {
+static unsigned int hmc5883l_i2c_send(unsigned char cmd) {
     return i2c_send(HMC5883L_ADDR, cmd);
 }
 
-static int hmc5883l_setup_reg(unsigned char v) {
+static unsigned int hmc5883l_setup_reg(unsigned char regaddr, unsigned char val) {
+	unsigned int ret;
+
+	ret = hmc5883l_i2c_send(regaddr);
+	if (!ret) {
+		/* fail */
+		return 0;
+	}
+	
+	ret = i2c_write(val);
+	i2c_stop();
+	return ret;
 }
 
 /*          R e g i s t e r  A          */
 
 /* # of sample per measurement output */
-static int hmc5883l_setup_cra(unsigned char ma, 
-							  unsigned char dor,
-							  unsigned char ms) {
-	int ret = 1;
+static unsigned int hmc5883l_setup_cra(unsigned char ma, 
+									   unsigned char dor,
+									   unsigned char mm) {
+	unsigned char val = 0;
 
-	/* TODO */
-	hmc5883l_i2c_send(HMC5883L_ADDR_CRA);
-
-
-	return ret;
+	val |= ((ma << CRA_MA_SHIFT) | (dor << CRA_DOR_SHIFT) | (mm << CRA_MM_SHIFT));
+	return hmc5883l_setup_reg(HMC5883L_ADDR_CRA, val);
 }
 
 /*        R e g i s t e r  B          */
-static int hmc5883l_setup_crb(unsigned char gn) {
-	int ret = 1;
-	
-	/* TODO */
+static unsigned int hmc5883l_setup_crb(unsigned char gn) {
+	unsigned char val = 0;
 
-	return ret;
+	val |= (gn << CRB_GN_SHIFT);
+	return hmc5883l_setup_reg(HMC5883L_ADDR_CRB, val);
 }
 
-/*          
- *   M o d e  R e g i s t e r   
- *
- *   continuous mode
- *   single mode
- *   idle mode 
- */
-static int hmc5883l_setup_md(unsigned char md) {
-	int ret = 1;
-	
-	/* TODO */
+/*      M o d e  R e g i s t e r      */
+static unsigned int hmc5883l_setup_mr(unsigned char md) {
+	unsigned char val = 0;
 
-	return ret;
+	val |= (md << MR_MD_SHIFT);
+	return hmc5883l_setup_reg(HMC5883L_ADDR_MR, val);
+}
+
+static unsigned int hmc5883l_read_sr(void) {
+	unsigned int ret;
+
+	ret = hmc5883l_i2c_send(HMC5883L_READ);
+	/* TODO */
 }
 
 int hmc5883l_test(void) {
-	int ret = 1;
+	unsigned int ret;
 
 	printf("\n%s initialized...\n", __FUNCTION__);
 	/* continuous-measurement mode */
+	ret = hmc5883l_setup_cra(CRA_MA_8, CRA_DOR_15, CRA_MM_NORMAL);
+	if (!ret) {
+		printf("error hmc5883l_setup_cra\n");
+		return 0;
+	}
 
-	
+	ret = hmc5883l_setup_crb(CRB_GN_5);
+	if (!ret) {
+		printf("error hmc5883l_setup_crb\n");
+		return 0;
+	}
+
+	ret = hmc5883l_setup_mr(MR_MD_CONTINUOUS);
+	if (!ret) {
+		printf("error hmc5883l_setup_mr\n");
+		return 0;
+	}
+
 	
 	/* single-measurement mode */
 
