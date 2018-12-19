29d9e94ba9cadf1178aa8ab4e81b8ab43c375753
Jeho Park <parkjeho@swmobile.co.kr>
2018-12-19 23:48:22 +0900

    delete null line

--- i2c.h
+++ i2c.h
@@ -108,46 +108,33 @@
  @return none
  */
 extern void i2c_init(void);
-
-
 /** 
  @brief Terminates the data transfer and releases the I2C bus 
  @param void
  @return none
  */
 extern void i2c_stop(void);
-
-
 /** 
  @brief Issues a start condition and sends address and transfer direction 
-  
  @param    addr address and transfer direction of I2C device
  @retval   0   device accessible 
  @retval   1   failed to access device 
  */
 extern unsigned char i2c_start(unsigned char addr);
-
-
 /**
  @brief Issues a repeated start condition and sends address and transfer direction 
-
  @param   addr address and transfer direction of I2C device
  @retval  0 device accessible
  @retval  1 failed to access device
  */
 extern unsigned char i2c_rep_start(unsigned char addr);
-
-
 /**
  @brief Issues a start condition and sends address and transfer direction 
-   
  If device is busy, use ack polling to wait until device ready 
  @param    addr address and transfer direction of I2C device
  @return   none
  */
 extern void i2c_start_wait(unsigned char addr);
-
- 
 /**
  @brief Send one byte to I2C device
  @param    data  byte to be transfered
@@ -155,25 +142,19 @@ extern void i2c_start_wait(unsigned char addr);
  @retval   1 write failed
  */
 extern unsigned char i2c_write(unsigned char data);
-
-
 /**
  @brief    read one byte from the I2C device, request more data from device 
  @return   byte read from I2C device
  */
 extern unsigned char i2c_readAck(void);
-
 /**
  @brief    read one byte from the I2C device, read is followed by a stop condition 
  @return   byte read from I2C device
  */
 extern unsigned char i2c_readNak(void);
-
 /** 
  @brief    read one byte from the I2C device
- 
  Implemented as a macro, which calls either i2c_readAck or i2c_readNak
- 
  @param    ack 1 send ack, request more data from device<br>
                0 send nak, read is followed by a stop condition 
  @return   byte read from I2C device
@@ -183,9 +164,7 @@ extern unsigned char i2c_read(unsigned char ack);
 
 
 /**@}*/
-
 extern unsigned int i2c_send(char addr, char cmd);
-
 
 int8_t i2c_readBytes(uint8_t dev, uint8_t regAddr, uint8_t length, uint8_t *data);
 int8_t i2c_readByte(uint8_t dev, uint8_t regAddr, uint8_t *data);
