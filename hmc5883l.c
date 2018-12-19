f1d7b1f9973a93ae30399828f1e309ceebd56f99
Jeho Park <parkjeho@swmobile.co.kr>
2018-12-19 23:48:46 +0900

    stolen fromm hmc5883 code from external :-)

--- hmc5883l.c
+++ hmc5883l.c
@@ -13,322 +13,105 @@
 #include "hmc5883l.h"
 #include <math.h>
 
-static unsigned int hmc5883l_i2c_send(unsigned char cmd) {
-    return i2c_send(HMC5883L_ADDR, cmd);
+uint8_t hmcReadRegister(uint8_t regAddr) {
+  uint8_t data;
+
+  i2c_readByte(HMC5883_SLAVE_ADDR, regAddr, &data);
+  return data;
 }
 
-/* success: 1
- * fail: 0
- */
-static unsigned int hmc5883l_setup_reg(unsigned char reg, unsigned char val) {
-	unsigned int ret;
+void hmcWriteRegister(uint8_t regAddr, uint8_t data) {
 
-	ret = hmc5883l_i2c_send(reg);
-	if (!ret) {
-		/* fail */
-		return 0;
-	}
-	
-	ret = i2c_write(val);
-	if (ret) {
-		return 0;
-	}
-	i2c_stop();
-
-	return 1;
+  i2c_writeByte(HMC5883_SLAVE_ADDR, regAddr, data);
 }
 
-/*          R e g i s t e r  A          */
+void hmcInit() {
+  uint8_t cfgA_data, cfgB_data;
 
-/* # of sample per measurement output */
-static unsigned int hmc5883l_setup_cra(unsigned char ma, 
-									   unsigned char dor,
-									   unsigned char mm) {
-	unsigned char val = 0;
-
-	val |= ((ma << CRA_MA_SHIFT) | (dor << CRA_DOR_SHIFT) | (mm << CRA_MM_SHIFT));
-	return hmc5883l_setup_reg(HMC5883L_ADDR_CRA, val);
+  cfgA_data = HMC_UPDATE_RATE | HMC_MODE_NORMAL | HMC_SAMPLE_RATE;
+  cfgB_data = HMC_GAIN_1090;
+  // set update rate, measurement mode and sample rate
+  hmcWriteRegister(HMC5883_REG_CONFIG_A, cfgA_data);
+  // set device gain
+  hmcWriteRegister(HMC5883_REG_CONFIG_B, cfgB_data);
+  // set operating mode of the device
+  hmcWriteRegister(HMC5883_REG_MODE, HMC_MODE );
 }
 
-/*        R e g i s t e r  B          */
-static unsigned int hmc5883l_setup_crb(unsigned char gn) {
-	unsigned char val = 0;
+uint16_t hmcGetAxisComponent(uint8_t axis) {
+  uint8_t data[2];
+  uint16_t value;
 
-	val |= (gn << CRB_GN_SHIFT);
-	return hmc5883l_setup_reg(HMC5883L_ADDR_CRB, val);
+  switch(axis)
+  {
+
+	case X_AXIS:
+	  data[0] = hmcReadRegister(HMC5883_REG_DATAX_H);
+	  data[1] = hmcReadRegister(HMC5883_REG_DATAX_L);
+	  value = (data[0] << 8) | data[1];
+	  break;
+
+	case Y_AXIS:
+	  data[0] = hmcReadRegister(HMC5883_REG_DATAY_H);
+	  data[1] = hmcReadRegister(HMC5883_REG_DATAY_L);
+	  value = (data[0] << 8) | data[1];
+	  break;
+
+	case Z_AXIS:
+	  data[0] = hmcReadRegister(HMC5883_REG_DATAZ_H);
+	  data[1] = hmcReadRegister(HMC5883_REG_DATAZ_L);
+	  value = (data[0] << 8) | data[1];
+	  break;
+  }
+
+  return value;
 }
 
-/*      M o d e  R e g i s t e r      */
-static unsigned int hmc5883l_setup_mr(unsigned char md) {
-	unsigned char val = 0;
-
-	val |= (md << MR_MD_SHIFT);
-	return hmc5883l_setup_reg(HMC5883L_ADDR_MR, val);
+uint32_t hmcGetHeadingAngle(void) {
+  uint16_t comp[3];
+  uint32_t angle;
+  // read X axis component
+  comp[0] = hmcGetAxisComponent(X_AXIS);
+  // read Y axis component
+  comp[1] = hmcGetAxisComponent(Y_AXIS);
+  // read Z axis component
+  comp[2] = hmcGetAxisComponent(Z_AXIS);
+  // calculate heading angle.Function atan2 defined in math.h
+  angle = (((atan2((int16_t)comp[0] ,(int16_t)comp[1])) * 180) / PI) + 180;
+  return angle;
 }
 
-/*
- * fail: 0
- * success: 0b11
- */
-
-unsigned char hmc5883l_read(unsigned char reg) {
-   unsigned char val = 0;
-
-   i2c_start(HMC5883L_WRITE);
-   i2c_write(reg);
-   i2c_rep_start(HMC5883L_READ);
-   val = i2c_readNak();
-   i2c_stop();
-   return (val);
-}
-
-
-static int hmc5883l_write(unsigned char reg, unsigned char value) {
-
-	i2c_start(HMC5883L_WRITE);
-	i2c_write(reg);
-	i2c_write(value);
-	i2c_stop();
-	return 0;
-}
-
-static int hmc5883l_verify() {
-	unsigned char v;
-#if 0	
-	if ((v = hmc5883l_read(HMC5883L_ADDR_IRA)) != IRA) {
-		dprintf("failed to read IRA(%c, %d)\n", v, v);
-		//return -1;
-	}
-	if ((v = hmc5883l_read(HMC5883L_ADDR_IRB)) != IRB) {
-		dprintf("failed to read IRB(%c)\n", v);
-		//return -2;
-	}
-	if ((v = hmc5883l_read(HMC5883L_ADDR_IRC)) != IRC) {
-		dprintf("failed to read IRC(%c)\n", v);
-		//return -3;
-	}
-
-	return 1;
-#else
-	i2c_start(0x3C);
-	i2c_write(HMC5883L_ADDR_IRA);
-	i2c_rep_start(HMC5883L_READ);
-	v = i2c_readNak();
-	i2c_stop();
-	dprintf("--->%c\n", v);
-	if ((int)v != IRA) {
-		return -1;
-	}
-
-	dprintf("--->%c\n", v);
-	return 1;
-
-#endif
-}
-
-static int hmc5883l_init() {
-	int ret;
-
-	ret = hmc5883l_write(0x0, 0x70); /* 8 avg, 15Hz, normal op */
-	if (ret)
-		return ret;
-	dprintf("%d ");
-	ret = hmc5883l_write(0x1, 0xA0); /* G=5 */
-	if (ret)
-		return ret;
-	dprintf("%d ");
-	ret = hmc5883l_write(0x2, 0x00); /* 8 avg, 15Hz, normal op */
-	if (ret)
-		return ret;
-	dprintf("%d ");
-
-	return ret;
-}
-
-/* 
- * fail: 0
- * success: status, ready
- */
-static unsigned char hmc5883l_wait_data() {
-	unsigned char status;
-
-	do {
-		status = hmc5883l_read(HMC5883L_ADDR_SR);
-		if (!status) {
-			return 0;
-		}
-
-		if (status & (1 << SR_LOCK_SHIFT))  {
-			/* now, locked */
-			_delay_us(250);
-			continue;
-		}
-
-		if (!(status & (1 << SR_RDY_SHIFT)))  {
-			/* ready */
-			_delay_us(250);
-			continue;
-		}
-		
-		/* now, no locked and ready to be read */
-		break;
-
-	} while (1);
-
-	return status;
-}
-
-void Magneto_init()		/* Magneto initialize function */
-{
-	dprintf("\n");
-	i2c_start(0x3C);	/* start and write SLA+W */
-	i2c_write(0x00);	/* Write memory location address */
-	i2c_write(0x70);	/* Configure register A as 8-average, 15 Hz default, normal measurement */
-	i2c_write(0xA0);	/* Configure register B for gain */
-	i2c_write(0x00);	/* Configure continuous measurement mode in mode register */
-	i2c_stop();		/* Stop i2c */
-	dprintf("\n");
-}
-
-#define PI	3.14159265359	 /* Define Pi value */
-#define Declination	-0.00669 /* Define declination of location from where measurement going to be done. 
-								e.g. here we have added declination from location Pune city, 
-								India. we can get it from http://www.magnetic-declination.com */
-int Magneto_GetHeading() {
-	int x, y, z;
-	double Heading;
-
-	dprintf("\n");
-	//i2c_start_wait(0x3C);	/* start and wait for acknowledgment */
-	i2c_start(0x3C);	/* start and wait for acknowledgment */
-	i2c_write(0x03);	/* Write memory location address */
-	i2c_rep_start(0x3D);/* Generate repeat start condition with SLA+R */
-	/* Read 16 bit x,y,z value (2's complement form) */
-	x = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
-	z = (((int)i2c_readAck()<<8) | (int)i2c_readAck());
-	y = (((int)i2c_readAck()<<8) | (int)i2c_readNak());
-	i2c_stop();		/* Stop i2c */
-	Heading = atan2((double)y,(double)x) + Declination;
-	if (Heading>2*PI)	/* Due to declination check for >360 degree */
-		Heading = Heading - 2*PI;
-	if (Heading<0)		/* Check for sign */
-		Heading = Heading + 2*PI;
-	
-	//return (Heading* 180 / PI);/* Convert into angle and return */
-	dprintf("---> %3.0f \n", Heading* 180 / PI);/* Convert into angle and return */
-	return 1;
+uint32_t hmcGetMagneticFieldStrength(uint8_t axis) {
+  uint16_t axisVal;
+  uint32_t fieldStrength;
+  // read axis component
+  axisVal = hmcGetAxisComponent(axis);
+  // multiply raw value by scaling factor
+  fieldStrength = axisVal * SCALING_FACTOR;
+  return fieldStrength;
 }
 
 /* continuous mode */
 int hmc5883l_test(void) {
-#if defined(HMC5883L_TEST)
-	int ret;
-	int16_t x, z, y;
-#if 1
+#ifdef HMC5883_TEST
+	uint32_t angle;
+	uint32_t x_fs;
+	uint32_t y_fs;
+	uint32_t z_fs;
 
-	dprintf("");
-    //hmc5883l_init(); 
-	Magneto_init();
-	dprintf("");
-	if (hmc5883l_verify() < 0) {
-		return -1;
-	}
-	dprintf("");
-	return 1;
+	hmcInit();
 
-	while(1) {
-		Magneto_GetHeading();
-	}
+	for(;;) {
 
-	return 1;
-#endif
+		angle = hmcGetHeadingAngle();
+		x_fs = hmcGetMagneticFieldStrength(X_AXIS);
+		y_fs = hmcGetMagneticFieldStrength(Y_AXIS);
+		z_fs = hmcGetMagneticFieldStrength(Z_AXIS);
 
-#if 0
-	printf("\n%s initialized...\n", __FUNCTION__);
-	ret = hmc5883l_init();
-	if (ret) {
-		printf("failed to init(%d)\n", ret);
-		return ret;
-	} else {
-		printf("success to init\n");
+		printf("angle(%u), x(%lu), y(%lu), z(%lu)\r\n", angle, x_fs, y_fs, z_fs);
+		_delay_ms(1000);
 	}
 
-#else
-	/* continuous-measurement mode */
-	ret = hmc5883l_setup_cra(CRA_MA_8, CRA_DOR_15, CRA_MM_NORMAL);
-	if (!ret) {
-		printf("error hmc5883l_setup_cra\n");
-		return 0;
-	}
-
-	ret = hmc5883l_setup_crb(CRB_GN_5);
-	if (!ret) {
-		printf("error hmc5883l_setup_crb\n");
-		return 0;
-	}
-
-	ret = hmc5883l_setup_mr(MR_MD_CONTINUOUS);
-	if (!ret) {
-		printf("error hmc5883l_setup_mr\n");
-		return 0;
-	}
-#endif	
-	do {
-		printf("wait data ...\n");
-		/* 1. wait about 6ms or check SR */ 
-		hmc5883l_wait_data(); 
-
-		/* 2. 0x3D 0x06 : send cmd: which means "i will read 6 bytes" */
-		ret = i2c_start(HMC5883L_READ);
-		if ( ret ) {
-			//failed to issue start condition, possibly no device found */
-			i2c_stop();
-			printf("failed to set device addr %d\n", __LINE__);
-			return 0;
-		} else {
-			// issuing start condition ok, device accessible
-			// now pointer is point to 0x03 which is address of X 
-			ret = i2c_write(0x06); /* read 6 bytes */
-			i2c_stop();
-		}
-
-		/* 2. read 6 bytes */
-		ret = i2c_start(HMC5883L_READ);
-		if (ret) {
-			/* fail */
-			i2c_stop();
-			printf("Failed to start i2c (%d)\n", __LINE__);
-			return 0;
-		} else {
-			x = z = y = 0;
-			/* x, z, y, the size of each is 16bits */
-			x = i2c_readAck();
-			x <<= 8;
-			x |= i2c_readAck();
-
-			z = i2c_readAck();
-			z <<= 8;
-			z |= i2c_readAck();
-
-			y = i2c_readAck();
-			y <<= 8;
-			y |= i2c_readNak();
-			i2c_stop();
-		}
-
-		/* convert three 16-bit 2's compliment 
-		https://www.programiz.com/c-programming/bitwise-operators#complement
-		*/
-		x = (~x + 1) * -1;
-		z = (~z + 1) * -1;
-		y = (~y + 1) * -1;
-		printf("x(%d), y(%d), z(%d)\n", x, y, z);
-
-		hmc5883l_i2c_send(0x3);
-	} while (1);
-
-	return 1;
-
-#endif /* HMC5883L_TEST */
+#endif /* HMC5883_TEST */
 	return 1;
 }
--- hmc5883l.h
+++ hmc5883l.h
@@ -8,173 +8,40 @@
 #ifndef _HMC5883L_
 #define _HMC5883L_
 
-//#define HMC5883L_ADDR  0x1E
-#define HMC5883L_ADDR  0x3C
+// HMC5883 I2C slave address
+#define HMC5883_SLAVE_ADDR  0x3C
 
-#define HMC5883L_WRITE (HMC5883L_ADDR + I2C_WRITE)
-#define HMC5883L_READ  (HMC5883L_ADDR + I2C_READ)
+// HMC5883 register addresses
+#define HMC5883_REG_CONFIG_A  0x00
+#define HMC5883_REG_CONFIG_B  0x01
+#define HMC5883_REG_MODE      0x02
+#define HMC5883_REG_DATAX_H   0x03
+#define HMC5883_REG_DATAX_L   0x04
+#define HMC5883_REG_DATAZ_H   0x05
+#define HMC5883_REG_DATAZ_L   0x06
+#define HMC5883_REG_DATAY_H   0x07
+#define HMC5883_REG_DATAY_L   0x08
+// HMC5883 configuration
+// 3 dimensional axis
+#define X_AXIS  1
+#define Y_AXIS  2
+#define Z_AXIS  3
 
-/*
- * Register Address (8bits)
- */
-#define HMC5883L_ADDR_CRA     0x00  /* Configuration Register A */
-#define HMC5883L_ADDR_CRB     0x01  /* Configuration Register B */
-#define HMC5883L_ADDR_MR      0x02  /* Mode Register  */
-#define HMC5883L_ADDR_DOXMSBR 0x03  /* Data Output X MSB Register */
-#define HMC5883L_ADDR_DOXLSBR 0x04  /* Data Output X LSB Register */
-#define HMC5883L_ADDR_DOZMSBR 0x05  /* Data Output Z MSB Register */
-#define HMC5883L_ADDR_DOZLSBR 0x06  /* Data Output Z LSB Register */
-#define HMC5883L_ADDR_DOYMSBR 0x07  /* Data Output Y MSB Register */
-#define HMC5883L_ADDR_DOYLSBR 0x08  /* Data Output Y LSB Register */
-#define HMC5883L_ADDR_SR      0x09  /* Status Register */
-#define HMC5883L_ADDR_IRA     0x0A  /* Identification Regster A, 0b01001000 = 0x48 */
-#define HMC5883L_ADDR_IRB     0x0B  /* Identification Regster B, 0b00110100 = 0x34 */
-#define HMC5883L_ADDR_IRC     0x0C  /* Identification Regster C, 0b00110011 = 0x33 */
-
-/********************************************************************************** 
-  CRA (Configuration Registar A)
-  
-           [ CRA7  |  CRA6  |  CRA5  |  CRA4  |  CRA3  |  CRA2  |  CRA1  | CRA0   ]
-  default  [Resv(0)| MA1(0) | MA0(0) | DO2(1) | DO1(0) | DO0(0) | MS1(0) | MS0(0) ]
-
-            *Resv: Reserved
- ***********************************************************************************/
-/* 
-  MAx: number of samples averaged(1 ~ 8) per measurement output
-  MA1 MA0
-  0    0 : # of sample 1 (default) per output
-  0    1 :             2
-  1    0 :             4 ...
-*/
-#define CRA_MA_SHIFT  5
-#define CRA_MA_1	0x0     /* default, 1 samples averaged */
-#define CRA_MA_2	0x1     /* 2 samples */
-#define CRA_MA_4	0x2     /* 4 */
-#define CRA_MA_8	0x3     /* 8 */
-#define CRA_MA_DEFAULT CRA_MA_1
-/* 
-  DOxMSB: (Data Output Rate) (Hz)
-  DO2  DO1  DO0
-    0    0    0:  0.75(Hz)
-	0    0    1:  1.5(Hz) ...
-  ...
-*/
-#define CRA_DOR_SHIFT	2
-#define CRA_DOR_0	0x0     /* 0.75 (Hz) */
-#define CRA_DOR_1P5 0x1     /* 1.5       */
-#define CRA_DOR_3	0x2     /* 3         */
-#define CRA_DOR_7P5	0x3     /* 7.5       */
-#define CRA_DOR_15	0x4     /* default, 15  */
-#define CRA_DOR_30	0x5     /* 30        */
-#define CRA_DOR_75	0x6     /* 75        */
-#define CRA_DOR_DEFAULT CRA_DOR_15
-/*
-  MM(Measurement Mode)
-  MS1  MS0
-    0    0: Normal mode ...
-*/
-#define CRA_MM_SHIFT    0
-#define CRA_MM_NORMAL 0x0  /* Normal Mlode */
-#define CRA_MM_PB     0x1  /* Positive Bias config for X, Y and Z axes */
-#define CRA_MM_NB     0x2  /* Negative Bias config for X, Y and Z axes */
-
-/********************************************************************************** 
-  CRB (Configuration Registar B)
-         [ CRB7   |  CRB6  |  CRB5  |  CRB4  |  CRB3  |  CRB2  |  CRB1  | CRB0  ]
- default [ GN2(0) | GN1(0) | GN0(1) |   (0)  |   (0)  |   (0)  |  (0)   | (0)   ]
- ***********************************************************************************/
-/* 
-  GN (Gain Configuration Bit)
-  GN2  GN1  GN0
-    0    0    0:  +/-0.88Ga |  1370Gain (LSb/Gauss) | 0.73Digial Resolution(mG/LSb) 
-    0    0    1:   +/-1.3Ga |  1090Gain (LSb/Gauss) | 0.92Digial Resolution(mG/LSb) 
-	...
-	                                 LSb/Gause -> mG/LSb x 1000
-*/
-#define CRB_GN_SHIFT   5
-#define CRB_GN_0     0x0        /* +/- 0.88 Ga */
-#define CRB_GN_1     0x1        /* +/- 1.3 Ga */
-#define CRB_GN_2     0x2        /* +/- 1.9 Ga */
-#define CRB_GN_3     0x3        /* +/- 2.5 Ga */
-#define CRB_GN_4     0x4        /* +/- 4.0 Ga */
-#define CRB_GN_5     0x5        /* +/- 4.7 Ga */
-#define CRB_GN_6     0x6        /* +/- 5.6 Ga */
-#define CRB_GN_7     0x7        /* +/- 8.1 Ga */
-#define CRB_GN_DEFAULT CRB_GN_1 
-
-/********************************************************************************** 
-  
-  MR (Mode Register)
-
-          [ MR7   |  MR6  |  MR5  |  MR4  |  MR3 |  MR2  |  MR1   |  MR0   ]
-  default [ HS(0) |  (0)  |  (0)  |  (0)  |  (0) |  (0)  | MD1(0) | MD0(1) ]
-
- ***********************************************************************************/
-#define MR_MD_SHIFT          0
-#define MR_MD_CONTINUOUS   0x0   /* Continuous Measurement Mode */
-#define MR_MD_SINGLE       0x1   /* Single Measurement Mode */
-#define MR_MD_IDLE         0x2   /* Idle Mode */
-#define MR_MD_IDLE2        0x3   /* Idle Mode */ 
-#define MR_MD_DEFAULT      MR_MD_SINGLE
-
-/********************************************************************************** 
-  DXRAnB (Data Output X Registers A and B)
-  
-  [ DXRA7 | DXRA6  | DXRA5 | DXRA4 | DXRA3 | DXRA2 | DXRA1 | DXRA0 ]
-  [  (0)  |  (0)   |  (0)  |  (0)  |  (0)  |  (0)  |  (0)  |  (0)  ]
-  [ DXRB7 | DXRB6  | DXRB5 | DXRB4 | DXRB3 | DXRB2 | DXRB1 | DXRB0 ]
-  [  (0)  |  (0)   |  (0)  |  (0)  |  (0)  |  (0)  |  (0)  |  (0)  ]
-
-  DYRAnB (Data Output Y Registers A and B)
-  
-  [ DYRA7 | DYRA6  | DYRA5 | DYRA4 | DYRA3 | DYRA2 | DYRA1 | DYRA0 ]
-  [ DYRB7 | DYRB6  | DYRB5 | DYRB4 | DYRB3 | DYRB2 | DYRB1 | DYRB0 ]
-
-  DZRAnB (Data Output Z Registers A and B)
-  
-  [ DZRA7 | DZRA6  | DZRA5 | DZRA4 | DZRA3 | DZRA2 | DZRA1 | DZRA0 ]
-  [ DZRB7 | DZRB6  | DZRB5 | DZRB4 | DZRB3 | DZRB2 | DZRB1 | DZRB0 ]
-
- *********************************************************************************/
-
-/********************************************************************************** 
-  
-  SR (Status Register)
-
-          [ SR7  |  SR6  |  SR5  |  SR4  |  SR3 |  SR2  |  SR1   |  SR0   ]
-  default [  (0) |  (0)  |  (0)  |  (0)  |  (0) |  (0)  |LOCK(0) | RDY(0) ]
-
- ***********************************************************************************/
-#define HMC5883L_SR_READ      HMC5883L_ADDR_SR + I2C_READ
-#define SR_LOCK_SHIFT 1
-#define SR_RDY_SHIFT  0
-
-/********************************************************************************** 
-  Identification Register A
-
-          [IRA7|IRA6|IRA5|IRA4|IRA3|IRA2|IRA1|IRA0]
-  default [(0) |(1) |(0) |(0) |(1) |(0) |(0) |(0) ]
-
- ***********************************************************************************/
-#define IRA 0x48
-
-/********************************************************************************** 
-  Identification Register B
-
-          [IRA7|IRA6|IRA5|IRA4|IRA3|IRA2|IRA1|IRA0]
-  default [(0) |(0) |(1) |(1) |(0) |(1) |(0) |(0) ]
-
- ***********************************************************************************/
-#define IRB 0x34
-
-/********************************************************************************** 
-  Identification Register C
-
-          [IRA7|IRA6|IRA5|IRA4|IRA3|IRA2|IRA1|IRA0]
-  default [(0) |(0) |(1) |(1) |(0) |(0) |(1) |(1) ]
-
- ***********************************************************************************/
-#define IRC 0x33
+// HMC5883 configuration
+// update rate = 75 Hz
+#define HMC_UPDATE_RATE  0x18
+// measurement mode = normal (No positive/negative biasing)
+#define HMC_MODE_NORMAL  0x00
+// number of samples averaged per measurement output = 8
+#define HMC_SAMPLE_RATE  0x60
+// gain = 1090 LSb/Gauss
+#define HMC_GAIN_1090  0x01
+// disable high speed I2C, enable single measurement mode
+#define HMC_MODE  0x01
+// scaling factor for gain = 1090 LSb/Gauss
+#define SCALING_FACTOR  0.73
+// PI value
+#define PI  3.14159265358979323846
 
 extern int hmc5883l_test(void);
 
