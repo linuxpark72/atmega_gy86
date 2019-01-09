3331252bfe93d0f875ad3e844bb3396df2ad4b47
jeho.park <jeho.park@lgepartner.com>
2019-01-09 15:44:44 +0900

    add a routine to get accel, gyro raw data

--- mpu6050.c
+++ mpu6050.c
@@ -15,43 +15,43 @@
 #include "uart.h"
 #include "mpu6050.h"
 
-#define MPU6050_RA_CONFIG           0x1A
-#define MPU6050_RA_GYRO_CONFIG      0x1B
-#define MPU6050_GCONFIG_FS_SEL_BIT      4
-#define MPU6050_GCONFIG_FS_SEL_LENGTH   2
-#define MPU6050_GYRO_FS_250         0x00
-#define MPU6050_GYRO_FS_500         0x01
-#define MPU6050_GYRO_FS_1000        0x02
-#define MPU6050_GYRO_FS_2000        0x03
+static uint8_t buffer[7];
+static int16_t accADC[3];
+static int16_t gyroADC[3];
+static float   accRaw[3];      //m/s^2
+static float   gyroRaw[3];     //rad/s
 
-#define MPU6050_RA_ACCEL_CONFIG     0x1C
-#define MPU6050_RA_PWR_MGMT_1       0x6B
-#define MPU6050_RA_SMPLRT_DIV       0x19
-#define MPU6050_RA_INT_PIN_CFG      0x37
-#define MPU6050_RA_WHO_AM_I         0x75
-#define MPU6050_WHO_AM_I_BIT        6
-#define MPU6050_WHO_AM_I_LENGTH     6
+static void MPU6050AccRead(int16_t *accData) {
+	uint8_t buf[6];
 
-#define MPU6050_DLPF_BW_42          0x03
+	i2c_readBytes(0x68, MPU6050_RA_ACCEL_XOUT_H, 6, buf);
+	accData[0] = (int16_t)((buf[0] << 8) | buf[1]);
+	accData[1] = (int16_t)((buf[2] << 8) | buf[3]);
+	accData[2] = (int16_t)((buf[4] << 8) | buf[5]);
+}
 
-static uint8_t buffer[14];
+static void MPU6050GyroRead(int16_t *gyroData) {
+	uint8_t buf[6];
+
+	i2c_readBytes(0x68, MPU6050_RA_GYRO_XOUT_H, 6, buf);
+	gyroData[0] = (int16_t)((buf[0] << 8) | buf[1]) ;
+	gyroData[1] = (int16_t)((buf[2] << 8) | buf[3]) ;
+	gyroData[2] = (int16_t)((buf[4] << 8) | buf[5]) ;
+}
 
 /* stolen from crazepony-firmware-none, i2cdevlib */
 static void MPU6050_initialize() {
 
 	//PWR_MGMT_1    -- DEVICE_RESET 1
-	//IICwriteByte(devAddr, MPU6050_RA_PWR_MGMT_1, 0x80);
 	i2c_writeByte(0x68, MPU6050_RA_PWR_MGMT_1, 0x80);
 
 	//delay_ms(50);
 	_delay_ms(50);
 
 	//SMPLRT_DIV    -- SMPLRT_DIV = 0  Sample Rate = Gyroscope Output Rate / (1 + SMPLRT_DIV)
-	//IICwriteByte(devAddr, MPU6050_RA_SMPLRT_DIV, 0x00);
 	i2c_writeByte(0x68, MPU6050_RA_SMPLRT_DIV, 0x00);
 
     //PWR_MGMT_1    -- SLEEP 0; CYCLE 0; TEMP_DIS 0; CLKSEL 3 (PLL with Z Gyro reference)
-	//IICwriteByte(devAddr, MPU6050_RA_PWR_MGMT_1, 0x03);
 	i2c_writeByte(0x68, MPU6050_RA_PWR_MGMT_1, 0x03);
 
 	// INT_PIN_CFG
@@ -59,22 +59,17 @@ static void MPU6050_initialize() {
 	// -- INT_LEVEL_HIGH, INT_OPEN_DIS, LATCH_INT_DIS, INT_RD_CLEAR_DIS, 
 	//        (3)                  (2)               (1)         (0)
 	//    FSYNC_INT_LEVEL_HIGH, FSYNC_INT_DIS, I2C_BYPASS_EN, CLOCK_DIS
-	//
-	//IICwriteByte(devAddr, MPU6050_RA_INT_PIN_CFG, 0 << 7 | 0 << 6 | 0 << 5 | 0 << 4 | 0 << 3 | 0 << 2 | 1 << 1 | 0 << 0);
 	i2c_writeByte(0x68, MPU6050_RA_INT_PIN_CFG, 0 << 7 | 0 << 6 | 0 << 5 | 0 << 4 | 0 << 3 | 0 << 2 | 1 << 1 | 0 << 0);
 
 	//CONFIG        
 	//-- EXT_SYNC_SET 0 (disable input pin for data sync) ; 
 	//default DLPF_CFG = 0 => ACC bandwidth = 260Hz  GYRO bandwidth = 256Hz)
 	//IICwriteByte(devAddr, MPU6050_RA_CONFIG, MPU6050_DLPF_BW_42);
-	// 
 	// Digital Low Pass Filter(DLPF)
-	//
 	//                       ACCEL (Fs 1kHz)     Gyro (Fs 1kHz)
 	//                       bw    delay         bw    delay
 	//MPU6050_DLPF_BW_42(3): 44Hz  4.9ms         42Hz  4.8ms 
 	i2c_writeByte(0x68, MPU6050_RA_CONFIG, MPU6050_DLPF_BW_42);
-
 	//MPU6050_setFullScaleGyroRange(MPU6050_GYRO_FS_2000);
 	//void MPU6050_setFullScaleGyroRange(uint8_t range) {
 	//    IICwriteBits(devAddr, MPU6050_RA_GYRO_CONFIG, MPU6050_GCONFIG_FS_SEL_BIT, MPU6050_GCONFIG_FS_SEL_LENGTH, range);
@@ -87,10 +82,7 @@ static void MPU6050_initialize() {
 }
 
 uint8_t MPU6050_getDeviceID() {
-	
-	//I2Cdev_readBits(mpu6050.devAddr, MPU6050_RA_WHO_AM_I, MPU6050_WHO_AM_I_BIT, MPU6050_WHO_AM_I_LENGTH, mpu6050.buffer);
 	i2c_readBits(0x68, MPU6050_RA_WHO_AM_I, MPU6050_WHO_AM_I_BIT, MPU6050_WHO_AM_I_LENGTH, buffer); 
-
 	return buffer[0];
 }
 
@@ -100,12 +92,31 @@ uint8_t MPU6050_testConnection() {
 
 int mpu6050_test() {
 #ifdef MPU6050_TEST
-	printf("\r\n mpu6050\r\n");
+	int i;
+
+	if (!MPU6050_testConnection()) {
+		return -1;
+	}
+	printf("\r\nmpu6050(0x%x) connected!\r\n", MPU6050_getDeviceID() << 1);
+	_delay_ms(2000);
 
 	/* init */
 	MPU6050_initialize();
-	printf("mpu6050: %s\r\n", MPU6050_testConnection() ? "connected":"disconnected");
+	
+	/* load */
+	while(1) {
+		MPU6050AccRead(accADC);
+		MPU6050GyroRead(gyroADC);
 
+		for (i=0; i<3; i++) {
+			accRaw[i]= (float)accADC[i] *ACC_SCALE * CONSTANTS_ONE_G ;
+			gyroRaw[i]=(float)gyroADC[i] * GYRO_SCALE * M_PI_F /180.f;      //deg/s
+		}
+
+		printf("ACCEL[x:%4.3f, y:%4.3f, z:%4.3f], GYRO[x:%4.3f, y:%4.3f z:%4.3f]\r\n",
+			   accRaw[0], accRaw[1], accRaw[2], gyroRaw[0], gyroRaw[1], gyroRaw[2]);
+		_delay_ms(10);
+	}
 
 #endif /* MPU6050_TEST */
 	return 0;
--- mpu6050.h
+++ mpu6050.h
@@ -7,5 +7,54 @@
  */
 #ifndef _MPU6050_
 #define _MPU6050_
+
+
+#define MPU6050_RA_CONFIG           0x1A
+#define MPU6050_RA_GYRO_CONFIG      0x1B
+#define MPU6050_GCONFIG_FS_SEL_BIT      4
+#define MPU6050_GCONFIG_FS_SEL_LENGTH   2
+#define MPU6050_GYRO_FS_250         0x00
+#define MPU6050_GYRO_FS_500         0x01
+#define MPU6050_GYRO_FS_1000        0x02
+#define MPU6050_GYRO_FS_2000        0x03
+
+#define MPU6050_RA_ACCEL_CONFIG     0x1C
+#define MPU6050_RA_PWR_MGMT_1       0x6B
+#define MPU6050_RA_SMPLRT_DIV       0x19
+#define MPU6050_RA_INT_PIN_CFG      0x37
+#define MPU6050_RA_WHO_AM_I         0x75
+#define MPU6050_WHO_AM_I_BIT        6
+#define MPU6050_WHO_AM_I_LENGTH     6
+
+#define MPU6050_RA_ACCEL_XOUT_H     0x3B
+#define MPU6050_RA_ACCEL_XOUT_L     0x3C
+#define MPU6050_RA_ACCEL_YOUT_H     0x3D
+#define MPU6050_RA_ACCEL_YOUT_L     0x3E 
+#define MPU6050_RA_ACCEL_ZOUT_H     0x3F 
+#define MPU6050_RA_ACCEL_ZOUT_L     0x40 
+#define MPU6050_RA_TEMP_OUT_H       0x41
+#define MPU6050_RA_TEMP_OUT_L       0x42
+#define MPU6050_RA_GYRO_XOUT_H      0x43
+#define MPU6050_RA_GYRO_XOUT_L      0x44
+#define MPU6050_RA_GYRO_YOUT_H      0x45
+#define MPU6050_RA_GYRO_YOUT_L      0x46
+#define MPU6050_RA_GYRO_ZOUT_H      0x47
+#define MPU6050_RA_GYRO_ZOUT_L      0x48
+
+#define MPU6050_DLPF_BW_42          0x03
+
+#define M_PI_F 3.1415926
+#define CONSTANTS_ONE_G                         9.80665f        /* m/s^2        */
+#define CONSTANTS_AIR_DENSITY_SEA_LEVEL_15C     1.225f          /* kg/m^3       */
+#define CONSTANTS_AIR_GAS_CONST                 287.1f          /* J/(kg * K)   */
+#define CONSTANTS_ABSOLUTE_NULL_CELSIUS         -273.15f        /* 癈           */
+#define CONSTANTS_RADIUS_OF_EARTH               6371000         /* meters (m)   */
+
+//constant g // tobe fixed to 8g. but IMU need to correct at the same time
+#define SENSOR_MAX_G 8.0f
+#define SENSOR_MAX_W 2000.0f    //deg/s
+#define ACC_SCALE  (SENSOR_MAX_G/32768.0f)
+#define GYRO_SCALE  (SENSOR_MAX_W/32768.0f)
+
 extern int mpu6050_test();
 #endif /* _MPU6050_ */
